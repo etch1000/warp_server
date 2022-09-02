72a96480f8e71340d66fe7ad7f35870df3dd3862
etch1000 <110674620+etch1000@users.noreply.github.com>
2022-09-02 12:01:40 +0530

    Implemented Basic Warp Server For Todo Application

---
+++ src/todo_rest.rs
@@ -0,0 +1,75 @@
+use crate::security::{do_auth, UserCtx};
+use crate::{with_db_pool, DbPool};
+use serde_json::{json, Value};
+use std::sync::Arc;
+use warp::{reply::Json, Filter};
+
+pub fn todos_filter(
+    db_pool: Arc<DbPool>,
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    let todos_base = warp::path("todos");
+    // List Todos
+    let list = todos_base
+        .and(warp::get())
+        .and(warp::path::end())
+        .and(do_auth())
+        .and(with_db_pool(db_pool.clone()))
+        .and_then(todo_list);
+
+    let get = todos_base
+        .and(warp::get())
+        .and(do_auth())
+        .and(with_db_pool(db_pool.clone()))
+        .and(warp::path::param()) // eg. /todos/123
+        .and_then(todo_get);
+
+    let create = todos_base
+        .and(warp::post())
+        .and(do_auth())
+        .and(with_db_pool(db_pool.clone()))
+        .and(warp::body::json())
+        .and_then(todo_create);
+
+    list.or(get).or(create)
+}
+
+async fn todo_list(_user_ctx: UserCtx, _db_pool: Arc<DbPool>) -> Result<Json, warp::Rejection> {
+    // TODO - get from DB
+    let todos = json!([
+        {"id": 1, "title": "todo 1"},
+        {"id": 2, "title": "todo 2"},
+    ]);
+
+    let todos = warp::reply::json(&todos);
+
+    Ok(todos)
+}
+
+async fn todo_get(
+    _user_ctx: UserCtx,
+    _db_pool: Arc<DbPool>,
+    id: i64,
+) -> Result<Json, warp::Rejection> {
+    // TODO - get from DB
+    let todo = json!(
+        {"id": id, "user_id": _user_ctx.user_id, "title": format!("todo {}", id)}
+    );
+
+    // serde-json to warp-reply
+    let todo = warp::reply::json(&todo);
+
+    Ok(todo)
+}
+
+async fn todo_create(
+    _user_ctx: UserCtx,
+    _db_pool: Arc<DbPool>,
+    data: Value,
+) -> Result<Json, warp::Rejection> {
+    // TODO - write to DB
+    let todo = data;
+
+    let todo = warp::reply::json(&todo);
+
+    Ok(todo)
+}
